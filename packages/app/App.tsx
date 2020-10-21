022d5b541ce4aa8a4d6f1850c9d48eb9da6419eb
Nilomiranda <nilomiranda3@gmail.com>
2020-10-20 21:08:08 -0300

    added react-navigation

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const MainStack = createStackNavigator();
 
 const App = () => {
-  return <Text>Hello</Text>;
+  return (
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <MainStack.Screen name="HomeScreen" component={HomeScreen} />
+      </MainStack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
