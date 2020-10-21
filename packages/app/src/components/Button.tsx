beac7631e810e6440697a8c8134cc50d7cd7d7a9
Nilomiranda <nilomiranda3@gmail.com>
2020-10-20 22:32:32 -0300

    creating button component

---
+++ packages/app/src/components/Button.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+type ButtonAppearance = 'primary' | 'secondary';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onPress?: () => void;
+  appearance?: ButtonAppearance;
+}
+
+const StyledButton = styled.TouchableOpacity<{ appearance: ButtonAppearance }>`
+  border-radius: 4px;
+  padding: 10px;
+  background: ${({ appearance }) =>
+    appearance === 'primary' ? 'black' : 'transparent'};
+  border: 2px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ButtonLabel = styled.Text<{ appearance: ButtonAppearance }>`
+  color: ${({ appearance }) => (appearance === 'primary' ? 'white' : 'black')};
+  font-weight: bold;
+`;
+
+const Button = ({
+  appearance = 'primary',
+  children,
+  onPress = () => null,
+}: ButtonProps) => {
+  return (
+    <StyledButton appearance={appearance} onPress={onPress}>
+      <ButtonLabel appearance={appearance}>{children}</ButtonLabel>
+    </StyledButton>
+  );
+};
+
+export default Button;
--- packages/app/src/modules/auth/SignInScreen.tsx
+++ packages/app/src/modules/auth/SignInScreen.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Input from '../../components/Input';
 import styled from 'styled-components/native';
 import Margin from '../../components/Margin';
+import Button from '../../components/Button';
+import { Text } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
@@ -16,8 +18,13 @@ const SignInScreen = () => {
       <Margin margin={{ top: 0, bottom: 30 }}>
         <Input label="Email" />
       </Margin>
-      <Margin>
+      <Margin margin={{ bottom: 30 }}>
         <Input label="Password" type="password" />
+      </Margin>
+      <Margin>
+        <Button>
+          <Text>Sign In</Text>
+        </Button>
       </Margin>
     </Container>
   );
